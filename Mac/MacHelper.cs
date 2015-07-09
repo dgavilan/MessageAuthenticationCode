6314101ea19f383f4c8eb1bfd5961962da05947c
dgavilan <derrick.gavilan@gmail.com>
2015-07-09 10:23:59 -0500

    Adding code for the first time

---
+++ Mac/MacHelper.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace Mac
+{
+    public class MacHelper
+    {
+        public string BuildMac(string key, string message)
+        {
+            // NOTE: Code taken from http://buchananweb.co.uk/security01i.aspx
+
+            /*
+             Security is handled by use of a Message Authentication Code (MAC).  The MAC is generated using 
+             * the Keyed-Hashing for Message Authentication (HMAC) algorithm (as defined in RFC 2104).  
+             * HMAC utilizes the MD5 Message-Digest Algorithm (as defined in RFC 1321) as the hash function.
+             */
+
+            var encoding = new ASCIIEncoding();
+            var keyByte = encoding.GetBytes(key);
+
+            var hmacmd5 = new HMACMD5(keyByte);
+            //var hmacsha1 = new HMACSHA1(keyByte);
+            //var hmacsha256 = new HMACSHA256(keyByte);
+            //var hmacsha384 = new HMACSHA384(keyByte);
+            //var hmacsha512 = new HMACSHA512(keyByte);
+
+            var messageBytes = encoding.GetBytes(message);
+            var hashMessage = hmacmd5.ComputeHash(messageBytes);
+
+            return ByteToString(hashMessage);
+
+            //hashmessage = hmacsha1.ComputeHash(messageBytes);
+
+            //this.hmac2.Text = ByteToString(hashmessage);
+
+            //hashmessage = hmacsha256.ComputeHash(messageBytes);
+
+            //this.hmac3.Text = ByteToString(hashmessage);
+
+            //hashmessage = hmacsha384.ComputeHash(messageBytes);
+
+            //this.hmac4.Text = ByteToString(hashmessage);
+
+            //hashmessage = hmacsha512.ComputeHash(messageBytes);
+
+            //this.hmac5.Text = ByteToString(hashmessage);
+        }
+
+        public static string ByteToString(byte[] buff)
+        {
+            string sbinary = "";
+            for (int i = 0; i < buff.Length; i++)
+            {
+                sbinary += buff[i].ToString("X2"); // hex format
+            }
+            return (sbinary);
+        }
+
+    }
+}
---
+++ Vertify/MacTests.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mac;
+using System.Net;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vertify
+{
+    [TestClass]
+    public class MacTest
+    {
+        [TestMethod]
+        public void CanGenerateValidMac()
+        {
+            /*
+             The text string that will be hashed is the concatenation of the 
+             * <requestor>, <session>, <timestamp>, <routing>, <member>, and <account> fields.  
+             * RFC 2104 calls for a shared secret password that is used in creating the MAC.  
+             * Vertifi and the home banking services provider mutually agree on a key 
+             * (a minimum of 16-bytes in length), and a mechanism and schedule for periodically 
+             * refreshing this key.
+            
+             * http://www.briangrinstead.com/blog/multipart-form-post-in-c
+             */
+
+            var mac = GenerateMac();
+
+            Assert.IsNotNull(mac);
+        }
+
+        [TestMethod]
+        public void SendRequest()
+        {
+            var postUrl = "http://testing.testing.com";
+            var contentType = "multipart/form-data";
+            var userAgent = "";
+
+            var formDataStream = new System.IO.MemoryStream();
+            var formDataBoundary = String.Format("----------{0:N}", Guid.NewGuid());
+            
+            var formData = new Dictionary<string, string>();
+            formData.Add("requestor", "requestor1");
+            formData.Add("session", "session1");
+            formData.Add("timestamp", DateTime.UtcNow.ToString());
+            formData.Add("routing", "routing1");
+            formData.Add("member", "member1");
+            formData.Add("account", "account1");
+            formData.Add("MAC", GenerateMac());
+
+            foreach (var data in formData)
+            {
+                var postData = string.Format("--{0}\r\nContent-Disposition: form-data; name=\"{1}\"\r\n\r\n{2}",
+                    formDataBoundary,
+                    data.Key,
+                    data.Value);
+
+                formDataStream.Write(Encoding.UTF8.GetBytes(postData), 0, Encoding.UTF8.GetByteCount(postData));
+            }
+
+            formDataStream.Position = 0;
+            byte[] formDataBytes = new byte[formDataStream.Length];
+            formDataStream.Read(formDataBytes, 0, formDataBytes.Length);
+            formDataStream.Close();
+
+            HttpWebRequest request = WebRequest.Create(postUrl) as HttpWebRequest;
+            
+            request.Method = "POST";
+            request.ContentType = contentType;
+            request.UserAgent = userAgent;
+            request.CookieContainer = new CookieContainer();
+            request.ContentLength = formDataBytes.Length;
+
+            using (Stream requestStream = request.GetRequestStream())
+            {
+                requestStream.Write(formDataBytes, 0, formDataBytes.Length);
+                requestStream.Close();
+            }
+
+            var response = request.GetResponse() as HttpWebResponse;
+        }
+
+        private string GenerateMac()
+        {
+            var requestor = "sample.user";
+            var session = "session1";
+            var timestamp = DateTime.UtcNow.ToString();
+            var routing = "routing1";
+            var member = "member1";
+            var account = "account1";
+            var sharedSecretPassword = "sharedSecretPassword";
+            var message = String.Format("{0}{1}{2}{3}{4}{5}", requestor, session, timestamp, routing, member, account);
+
+            var macHelper = new MacHelper();
+            var mac = macHelper.BuildMac(
+                key: sharedSecretPassword,
+                message: message);
+
+            return mac;
+        }
+    }
+}
